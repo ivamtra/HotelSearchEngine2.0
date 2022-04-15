af1df5a129368340ab7625e7c3dbb3811dace57d
KristjanLeo <krissi.leo@gmail.com>
2022-04-15 20:01:31 +0000

    Hotels clickable

--- src/main/java/hotelsearchengine/vidmot/IndexController.java
+++ src/main/java/hotelsearchengine/vidmot/IndexController.java
@@ -4,11 +4,19 @@ import hotelsearchengine.controllers.loginController;
 import hotelsearchengine.controllers.searchController;
 import hotelsearchengine.models.Hotel;
 import hotelsearchengine.storage.databaseHelper;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -38,6 +46,21 @@ public class IndexController implements Initializable {
             hotelContainer.setText(h.getName());
 
             Label hotelNameContainer = new Label();
+            hotelContainer.onMouseClickedProperty().set(new EventHandler<MouseEvent>() {
+                @Override
+                public void handle(MouseEvent e) {
+                    try {
+                        Parent root = FXMLLoader.load(getClass().getResource("hotel-view.fxml"));
+                        Stage stage = (Stage) ((Node) e.getSource()).getScene().getWindow();
+                        Scene scene = new Scene(root);
+                        stage.setScene(scene);
+                        stage.show();
+                    }  catch (IOException exception) {
+                        exception.printStackTrace();
+                    }
+                }
+            });
+
 
             mainContainer.getChildren().add(hotelContainer);
         }
